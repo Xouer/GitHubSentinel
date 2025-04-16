fb739b10c429dae7686d5043a6f0fdb657641670
wanggang <1966609309@qq.com>
2025-04-16 17:57:50 +0800

    提交0.2.1版本代码

--- src/config.py
+++ src/config.py
@@ -10,4 +10,4 @@ class Config:
             self.github_token = config.get('github_token')
             self.notification_settings = config.get('notification_settings')
             self.subscriptions_file = config.get('subscriptions_file')
-            self.update_interval = config.get('update_interval', 24 * 60 * 60)  # Default to 24 hours
+            self.update_interval = config.get('update_interval', 15)  # Default to 24 hours
